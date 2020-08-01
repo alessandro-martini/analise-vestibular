65b842a6a42b1592683808107fa08e2620ba83b3
Alessandro Martini <martini97@protonmail.ch>
2020-08-01 14:01:22 -0300

    Updates

---
+++ precache-manifest.549317e12540b904d6c75ba072b897c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f71225bba433062c84fc3886ece8bc3",
+    "url": "/analise-vestibular/index.html"
+  },
+  {
+    "revision": "12f34978849b820dda8d",
+    "url": "/analise-vestibular/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "580de00db0a87ad4c82f",
+    "url": "/analise-vestibular/static/js/2.e555ac23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/analise-vestibular/static/js/2.e555ac23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12f34978849b820dda8d",
+    "url": "/analise-vestibular/static/js/main.a63b27e3.chunk.js"
+  },
+  {
+    "revision": "8ad5dad92fc13b16072d",
+    "url": "/analise-vestibular/static/js/runtime-main.9a2b6d5e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/analise-vestibular/static/media/logo.5d5d9eef.svg"
+  }
+]);
